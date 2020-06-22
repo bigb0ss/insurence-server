cd615bcfa1e8252ee6c296ab7ec5e469861d6d7e
Shri Charan <36787916+bigb0ss@users.noreply.github.com>
2020-06-22 18:41:10 +0530

    Add files via upload

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
 import mysql.connector as sql
 from flask import Flask, jsonify
-
+from datetime import timedelta, datetime 
 import logging
 
 app = Flask(__name__)
@@ -25,30 +25,57 @@ def init_db():
 @app.route('/vehicle')
 def getVechile():
 
-	db_connection = init_db()
-	app.logger.info("Database Connection Established")
-	db_cursor = db_connection.cursor()
-	db_cursor.execute('SELECT * FROM private_vehicles')
-	table_rows = db_cursor.fetchall()
-	app.logger.info("Results fetched from Database")
-	result =[]
-	for t in table_rows:
-		row={}
-		row['id']=str(t[0])
-		row['name']=t[1]
-		row['reference']=t[2]
-		row['contact']=t[3]
-		row['policy_number']=t[4]
-		row['policy_type']=t[5]
-		row['vehicle_type']=t[6]
-		row['mail']=t[7]
-		row['address']=t[8]
-		row['date']= t[9]
+        db_connection = init_db()
+        app.logger.info("Database Connection Established")
+        db_cursor = db_connection.cursor()
+        db_cursor.execute('SELECT * FROM private_vehicles')
+        table_rows = db_cursor.fetchall()
+        app.logger.info("Results fetched from Database")
+        result =[]
+        for t in table_rows:
+                row={}
+                row['id']=str(t[0])
+                row['name']=t[1]
+                row['reference']=t[2]
+                row['contact']=t[3]
+                row['policy_number']=t[4]
+                row['policy_type']=t[5]
+                row['vehicle_type']=t[6]
+                row['mail']=t[7]
+                row['address']=t[8]
+                row['date']= t[9].strftime("%d-%m-%y")
+                if (t[9])>(datetime.now()+timedelta(days=7)).date():
+                        result.append(row)
+        app.logger.info("Results are processed ")
+        return jsonify(result)
 
-		result.append(row)
-	app.logger.info("Results are processed ")
-	return jsonify(result)
 
+@app.route('/renew_vehicle')
+def getRenewVechile():
+
+        db_connection = init_db()
+        app.logger.info("Database Connection Established")
+        db_cursor = db_connection.cursor()
+        db_cursor.execute('SELECT * FROM private_vehicles')
+        table_rows = db_cursor.fetchall()
+        app.logger.info("Results fetched from Database")
+        result =[]
+        for t in table_rows:
+                row={}
+                row['id']=str(t[0])
+                row['name']=t[1]
+                row['reference']=t[2]
+                row['contact']=t[3]
+                row['policy_number']=t[4]
+                row['policy_type']=t[5]
+                row['vehicle_type']=t[6]
+                row['mail']=t[7]
+                row['address']=t[8]
+                row['date']= t[9].strftime("%d-%m-%y")
+                if (t[9])<(datetime.now()+timedelta(days=7)).date():
+                        result.append(row)
+        app.logger.info("Results are processed ")
+        return jsonify(result)
 
 if __name__ == '__main__':
 	app.run()
