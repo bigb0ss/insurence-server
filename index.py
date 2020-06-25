f48b0f80c3fe38ab003b2eb9f4088b3643890e4c
Shri Charan <36787916+bigb0ss@users.noreply.github.com>
2020-06-25 20:43:35 +0530

    2 more api added

--- index.py
+++ index.py
@@ -138,5 +138,63 @@ def getRenewMVechile():
 
 
 
+@app.route('/fire_misc')
+def getFireMisc():
+
+        db_connection = init_db()
+        app.logger.info("Database Connection Established")
+        db_cursor = db_connection.cursor()
+        db_cursor.execute('SELECT * FROM fire_misc')
+        table_rows = db_cursor.fetchall()
+        app.logger.info("Results fetched from Database")
+        result =[]
+        for t in table_rows:
+                row={}
+                row['id']=str(t[0])
+                row['name']=t[1]
+                row['reference']=t[2]
+                row['contact']=t[3]
+                row['policy_number']=t[4]
+                row['policy_type']=t[5]
+                #row['vehicle_type']=t[6]
+                row['mail']=t[6]
+                row['address']=t[7]
+                row['date']= t[8].strftime("%d-%m-%y")
+                if (t[8])>(datetime.now()+timedelta(days=7)).date():
+                        result.append(row)
+        app.logger.info("Results are processed ")
+        return jsonify(result)
+
+
+@app.route('/renew_fire_misc')
+def getRenewFireMisc():
+
+        db_connection = init_db()
+        app.logger.info("Database Connection Established")
+        db_cursor = db_connection.cursor()
+        db_cursor.execute('SELECT * FROM fire_misc')
+        table_rows = db_cursor.fetchall()
+        app.logger.info("Results fetched from Database")
+        result =[]
+        for t in table_rows:
+                row={}
+                row['id']=str(t[0])
+                row['name']=t[1]
+                row['reference']=t[2]
+                row['contact']=t[3]
+                row['policy_number']=t[4]
+                row['policy_type']=t[5]
+                #row['vehicle_type']=t[6]
+                row['mail']=t[6]
+                row['address']=t[7]
+                row['date']= t[8].strftime("%d-%m-%y")
+                if (t[8])<(datetime.now()+timedelta(days=7)).date():
+                        result.append(row)
+        app.logger.info("Results are processed ")
+        return jsonify(result)
+
+
+
+
 if __name__ == '__main__':
 	app.run()
